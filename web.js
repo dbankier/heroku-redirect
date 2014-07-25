8702c7f43a5f9eba0071be1e157cd2e5657fe6b5
David Bankier <bankierd@gmail.com>
2014-07-25 14:38:22 +1000

    should redirect all not just get

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('*', function(request, response) {
+app.all('*', function(request, response) {
   response.redirect(process.env.NEW_BASE_URL + request.url)
 });
 
